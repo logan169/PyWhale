2325593f97f6b9c01c91351330418f5e060e3a08
logan169 <loganschwartz@hotmail.com>
2017-05-20 21:40:20 +0200

    PyWhale the python wrapper trading lib for whaleclub.co Rest api

---
+++ README.md
@@ -0,0 +1,55 @@
+# PyWhale
+
+## Python3 wrapper for whaleclub rest api
+
+! [pywhale logo](./whale2.png)
+
+This repo contains a python3 wrapper for whaleclub rest api. The main goal was to make it as simple as possible.
+
+For better user experience, I've paste/reformat whaleclub.co [api] (http://docs.whaleclub.co/#list-turbo-positions) documentation for each function doc string. Please refer to the link above for up to date documentation.
+
+This project was created on my own free time, so if you wish to support my work you could either:
+
+- Consider creating an account using my whaleclub's referral link, so you'll get a 30% deposit bonus: https://whaleclub.co/join/pnI1A
+- leave me a tip at (BTC): 19nm2SJW7zmNcePePW6pyZBh7FMcWZdYeY or (DASH) XpdDgn28F3GcwyBjHwb6yKXqJ9nYHbrdup
+
+## Installation
+
+    $ git clone
+    $ cd pywhale/app
+
+## How to use it
+
+After creating an account, get your API token from your API Settings panel which is available from the top right menu in your trading dashboard. You get one token for live trading and another for demo trading.
+
+Then complet every following files with their respective api key:
+
+- pywhale/app/BTC_demo_key.txt
+- pywhale/app/BTC_live_key.txt
+- pywhale/app/DASH_demo_key
+- pywhale/app/DASH_live_key
+
+#### Beware to not invert api key, I strongly advise that you verify twice that you've entered the right key in each file.
+
+## Usage
+
+    #Start ipython
+    $ ipython3
+    
+    #Import module
+    $ from PyWhale import PyWhale
+        
+    #Create a PyWhale() instance
+    $ pw = PyWhale()    
+
+    #Print help
+    $ print(pw.help())
+    
+    #Remove print output function
+    $ pw.verbose = False
+    
+    #Remove print output function
+    $ pw.verbose = False
+
+
+
---
+++ app/PyWhale.py
@@ -0,0 +1,124 @@
+# Author: Logan Schwartz
+# This file is part of pywhale.
+# pywhale is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pywhale is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public LICENSE along with krakenex. If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
+import json
+
+import sys
+from pywhale.connection.Api import Api
+from pywhale.trade.General import General
+from pywhale.trade.Live import Live
+from pywhale.trade.Turbo import Turbo
+
+
+class PyWhale(Api,General,Live,Turbo):
+	"""Whaleclub.co cryptocurrency Exchange API Pyhon Client"""
+
+	def __init__(self, start_url = 'https://api.whaleclub.co/v1/'):
+		Api.__init__(self) # inherit token from API class for our connection class instance
+		General.__init__(self) # inherit token from API class for our connection class instance
+		Live.__init__(self) # inherit token from API class for our connection class instance
+		Turbo.__init__(self) # inherit token from API class for our connection class instance
+
+		self.start_url = start_url
+		self.default_key = 'BTC_demo_key' #set the key that will be used when no value is given in key parameter
+		self.verbose = True # set to True if you get output twice
+	
+	def _checkResp(self, resp):
+		"""Check whenever an response return an error"""
+		parsed = json.loads(resp.text)
+		
+		# every thing is ok
+		if resp.status_code == 200 or resp.status_code == 201:
+			if self.verbose:
+				print (json.dumps(parsed, indent=4, sort_keys=True))
+			return parsed
+		
+		# we have an error
+		else:
+			
+			print ('\nOOps, somethings went Wrong!\n')
+			
+			try:
+				print (parsed['error']['name'])
+				print (parsed['error']['message'])
+			except:
+				print (parsed)
+
+	def _testSymbols(self,symb):
+		if symb != '' and len(symb.split(',')) > 5:
+			print ('Error, You can only request information for up to 5 elements at once. Lower your input number and retry\n')
+			return False
+		else:
+			return True
+
+	def _updateKey(self,key):	
+		
+		if key is None:
+			key = self.default_key
+		
+		#test if key parameter value is an accepted input
+		l = ['BTC_live_key', 'BTC_demo_key', 'DASH_live_key','DASH_demo_key']
+		if key in l:
+			i = l.index(key)
+			k = [self.BTC_live_key, self.BTC_demo_key, self.DASH_live_key, self.DASH_demo_key]
+			key = k[i]	
+		else:
+			print ("\nError, enter an acctepted value for key parameter, could either be 'BTC_live_key', 'BTC_demo_key', 'DASH_live_key' or 'DASH_demo_key' \n")
+			return (False,key)
+		
+		return (True,key)
+
+	@classmethod
+	def help(self,function_name = None):
+		"""Returns a list of all callable functions"""
+		help_message = """
+Available functions:
+-------------------
+
+help()			Returns a list of all callable functions.
+
+General:
+--------
+getMarkets()		Returns market information for one or more markets.
+getPrice()		Returns the current bid and ask prices for one or more markets.
+getBalance()		Returns information about your balance.
+getTransactions()	List transactions that have occurred on your account.
+
+Live:
+-----
+newPosition()		Submit a new position.
+getPosition()		Fetch information about an existing position.
+updatePosition()	Fetch information about an existing position.
+closePosition()		Close one or multiple active positions at market price.
+cancelPosition()	Cancel one or multiple pending positions.
+splitPosition()		Split an existing pending or active position.
+listPositions()		List positions.
+
+Turbo:
+------
+getTurboActiveContracts()	Fetch a list of currently active turbo contracts.
+createNewTurboPosition()	Open a new turbo position.
+getTurboPosition()		Fetch information about an existing turbo position.
+listturboPositions()		List turbo positions.
+
+You could type print(PyWhale.function_name.__doc__) to get more info about any function
+		"""
+		
+		print (help_message)
+		return 
+		
+
+
