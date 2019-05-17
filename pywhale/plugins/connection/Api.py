6c275edb61090d64222199a234c4b1034facc515
logan169 <logan1691987@gmail.com>
2019-05-17 16:56:34 -0400

    pep8 autoformat

--- pywhale/plugins/connection/Api.py
+++ pywhale/plugins/connection/Api.py
@@ -14,81 +14,85 @@
 # General Public LICENSE along with krakenex. If not, see
 # <http://www.gnu.org/licenses/gpl-3.0.txt>.
 
-import os, sys
+import os
+import sys
+
 
 class Api (object):
-	"""
-	Whaleclub.co cryptocurrency Exchange API Pyhon Client Connection Handler methods:
-	
-	"""
-	
-	def __init__(self, BTC_real_key='', BTC_demo_key='', DASH_real_key='', DASH_demo_key='', ETH_real_key='', LTC_real_key=''):
-		"""
-		Create an object with authentication information.
-		API Token could be find from your API Settings panel which is available from the top right menu in your trading dashboard.
+    """
+    Whaleclub.co cryptocurrency Exchange API Pyhon Client Connection Handler methods:
 
-		Args:
-		DASH_demo_key -- DASH API Token for demo mode
-		DASH_real_key -- DASH API Token for real mode
-		BTC_demo_key  -- BTC API Token for demo mode
-		BTC_real_key  -- BTC API Token for real mode
-		ETH_real_key  -- ETH API Token for real mode
-		LTC_real_key  -- LTC API Token for real mode
+    """
 
-		"""
-		self.BTC_demo_key = BTC_demo_key
-		self.BTC_real_key = BTC_real_key
-		self.DASH_demo_key = DASH_demo_key
-		self.DASH_real_key = DASH_real_key
-		self.ETH_real_key = ETH_real_key
-		self.LTC_real_key = LTC_real_key
-		self.root_path = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[0:-2])
+    def __init__(self, BTC_real_key='', BTC_demo_key='', DASH_real_key='', DASH_demo_key='', ETH_real_key='', LTC_real_key=''):
+        """
+        Create an object with authentication information.
+        API Token could be find from your API Settings panel which is available from the top right menu in your trading dashboard.
 
-		self.load_tokens()
+        Args:
+        DASH_demo_key -- DASH API Token for demo mode
+        DASH_real_key -- DASH API Token for real mode
+        BTC_demo_key  -- BTC API Token for demo mode
+        BTC_real_key  -- BTC API Token for real mode
+        ETH_real_key  -- ETH API Token for real mode
+        LTC_real_key  -- LTC API Token for real mode
 
-	def load_tokens(self):
-		"""
-		Load API token from files
-		BTC_demo_key.txt,BTC_real_key.txt,DASH_demo_key.txt,DASH_real_key.txt, ETH_real_key.txt, LTC_real_key.txt
-		
-		"""
+        """
+        self.BTC_demo_key = BTC_demo_key
+        self.BTC_real_key = BTC_real_key
+        self.DASH_demo_key = DASH_demo_key
+        self.DASH_real_key = DASH_real_key
+        self.ETH_real_key = ETH_real_key
+        self.LTC_real_key = LTC_real_key
+        self.root_path = '/'.join(os.path.dirname(
+            os.path.abspath(__file__)).split('/')[0:-2])
 
-		keys = []
+        self.load_tokens()
 
-		files = ["BTC_demo_key.txt",
-		"BTC_real_key.txt",
-		"DASH_demo_key.txt",
-		"DASH_real_key.txt",
-		"ETH_real_key.txt",
-		"LTC_real_key.txt",
-		]
+    def load_tokens(self):
+        """
+        Load API token from files
+        BTC_demo_key.txt,BTC_real_key.txt,DASH_demo_key.txt,DASH_real_key.txt, ETH_real_key.txt, LTC_real_key.txt
 
-		#open files and save token
-		for file in files:
-			filepath = '/'.join([self.root_path, 'api_keys', file])
-			f = open(filepath,'r')
-			for lines in file:
-				key = f.readline().rstrip('\n')
-				self.validate_key(file, key)
-				keys.append(key)
-				break	#only read 1st line
+        """
 
-		#update token with token loaded
-		self.BTC_demo_key,self.BTC_real_key,self.DASH_demo_key,self.DASH_real_key,self.ETH_real_key,self.LTC_real_key = keys
-		welcome_message = '\n'+'#'*49+'\n'+'#'*6+'          Welcome to PyWhale         '+'#'*6+'\n'+'#'*6+'   Python wrapper for whaleclub.co   '+'#'*6+'\n'+'#'*49+'\n\n'
-		message = """API token loaded, ready to trade!\ntype PyWhale.help() at anytime to see available functions"""
-		print (welcome_message + message)
+        keys = []
 
-	def validate_key(self, file, key):
-		"""
-		Validate that the key exists and is complet
-		
-		"""
+        files = ["BTC_demo_key.txt",
+                 "BTC_real_key.txt",
+                 "DASH_demo_key.txt",
+                 "DASH_real_key.txt",
+                 "ETH_real_key.txt",
+                 "LTC_real_key.txt",
+                 ]
 
-		#check if token exist and are complet, token length is 36 char long
-		if len(key) == 36:
-			return
-		
-		error_message = '''\nError, API token for {} is either missing or incomplet (length < 36 characters)\nCheck that you've correctly enter your API token in {}/api_keys/{}'''.format(file, self.root_path, file)
-		sys.exit(error_message)
+        # open files and save token
+        for file in files:
+            filepath = '/'.join([self.root_path, 'api_keys', file])
+            f = open(filepath, 'r')
+            for lines in file:
+                key = f.readline().rstrip('\n')
+                self.validate_key(file, key)
+                keys.append(key)
+                break  # only read 1st line
 
+        # update token with token loaded
+        self.BTC_demo_key, self.BTC_real_key, self.DASH_demo_key, self.DASH_real_key, self.ETH_real_key, self.LTC_real_key = keys
+        welcome_message = '\n'+'#'*49+'\n'+'#'*6+'          Welcome to PyWhale         ' + \
+            '#'*6+'\n'+'#'*6+'   Python wrapper for whaleclub.co   '+'#'*6+'\n'+'#'*49+'\n\n'
+        message = """API token loaded, ready to trade!\ntype PyWhale.help() at anytime to see available functions"""
+        print(welcome_message + message)
+
+    def validate_key(self, file, key):
+        """
+        Validate that the key exists and is complet
+
+        """
+
+        # check if token exist and are complet, token length is 36 char long
+        if len(key) == 36:
+            return
+
+        error_message = '''\nError, API token for {} is either missing or incomplet (length < 36 characters)\nCheck that you've correctly enter your API token in {}/api_keys/{}'''.format(
+            file, self.root_path, file)
+        sys.exit(error_message)
--- pywhale/plugins/trade/Turbo.py
+++ pywhale/plugins/trade/Turbo.py
@@ -16,228 +16,232 @@
 
 import requests
 
+
 class Turbo (object):
 
-	def getTurboActiveContracts (self,	key = None):
-		"""
-		Fetch a list of currently active turbo contracts.
-		This endpoint will return information about currently active contracts such as the purchase deadline and expiry time.
-		When you submit a new turbo position, it’ll be on one of the active contracts you get from here.
+    def getTurboActiveContracts(self,	key=None):
+        """
+        Fetch a list of currently active turbo contracts.
+        This endpoint will return information about currently active contracts such as the purchase deadline and expiry time.
+        When you submit a new turbo position, it’ll be on one of the active contracts you get from here.
 
-		arg:
-		----
-		key		string 		Optional. 	One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
+        arg:
+        ----
+        key		string 		Optional. 	One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
 
-		resp:
-		-----
-		id 			string		Unique ID for the contract.
-		type			string 		Can be 1min or 5min.
-		created_at		integer 	When the contract first became active.
-		purchase_deadline	integer 	Time before which a turbo position must be submitted to be included in the contract.
-		expires_at		integer		When the contract expires and turbo positions settle.
+        resp:
+        -----
+        id 			string		Unique ID for the contract.
+        type			string 		Can be 1min or 5min.
+        created_at		integer 	When the contract first became active.
+        purchase_deadline	integer 	Time before which a turbo position must be submitted to be included in the contract.
+        expires_at		integer		When the contract expires and turbo positions settle.
 
-		"""
+        """
 
-		#test key parameter value is an accepted input
-		test0 =  self._updateKey(key)
-		if test0[0] is True:
-			key = test0[1]
-		else:
-			return
+        # test key parameter value is an accepted input
+        test0 = self._updateKey(key)
+        if test0[0] is True:
+            key = test0[1]
+        else:
+            return
 
-		#Create request elements
-		url = self.start_url+'contracts'
-		h = {"Authorization":"Bearer "+key}
-		r = requests.get(url,headers=h)
+        # Create request elements
+        url = self.start_url+'contracts'
+        h = {"Authorization": "Bearer "+key}
+        r = requests.get(url, headers=h)
 
-		if self.verbose:
-			print ('\nListing available active turbo contract: \n')
+        if self.verbose:
+            print('\nListing available active turbo contract: \n')
 
-		return self._checkResp(r)
+        return self._checkResp(r)
 
-	def createNewTurboPosition(self, position_direction = None, market=None, position_type=None, size=None, key = None):
-		"""
-		Open a new turbo position.
-		This endpoint allows you to open a new turbo position.
-		All turbo positions are executed at the market price, so there is no entry price to set.
+    def createNewTurboPosition(self, position_direction=None, market=None, position_type=None, size=None, key=None):
+        """
+        Open a new turbo position.
+        This endpoint allows you to open a new turbo position.
+        All turbo positions are executed at the market price, so there is no entry price to set.
 
-		arg:
-		----
-		position_direction	string 		Required. 	Can be "long" or "short"
-		market 			string 		Required. 	Market where you want to create a position
-		position_type 		string 		Required. 	Contract type. Can be 1min or 5min
-		size                	integer 	Required. 	Turbo position’s size, in satoshis.
-		key			string 		Optional. 	One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
+        arg:
+        ----
+        position_direction	string 		Required. 	Can be "long" or "short"
+        market 			string 		Required. 	Market where you want to create a position
+        position_type 		string 		Required. 	Contract type. Can be 1min or 5min
+        size                	integer 	Required. 	Turbo position’s size, in satoshis.
+        key			string 		Optional. 	One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
 
-		resp:
-		-----
-		id 		string   	Unique ID for the turbo position.
-		contract_id	string	 	ID of the contract this turbo position belongs to.
-		direction 	string 	 	Can be long or short.
-		market 		string 		Market this turbo position was executed on.
-		state 		string 		Can be active, or closed.
-		size 		integer 	Position’s size, in satoshis.
-		entry_price 	number 		Price at which the turbo position was executed.
-		payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
-		close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
-		profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
-		created_at 	integer 	When the position was created.
-		closed_at 	integer 	When the position was closed. Appears only if the position is closed.
-		currency 	string 		Base currency.
+        resp:
+        -----
+        id 		string   	Unique ID for the turbo position.
+        contract_id	string	 	ID of the contract this turbo position belongs to.
+        direction 	string 	 	Can be long or short.
+        market 		string 		Market this turbo position was executed on.
+        state 		string 		Can be active, or closed.
+        size 		integer 	Position’s size, in satoshis.
+        entry_price 	number 		Price at which the turbo position was executed.
+        payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
+        close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
+        profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
+        created_at 	integer 	When the position was created.
+        closed_at 	integer 	When the position was closed. Appears only if the position is closed.
+        currency 	string 		Base currency.
 
-		"""
-		d = {}
+        """
+        d = {}
 
-		#test position_direction value
-		if position_direction is None or position_direction not in ['short','long']:
-			print ("\nError, position_direction parameter value should either be 'short','long' \n")
-			return
-		else:
-			d['direction'] = str(position_direction)
+        # test position_direction value
+        if position_direction is None or position_direction not in ['short', 'long']:
+            print(
+                "\nError, position_direction parameter value should either be 'short','long' \n")
+            return
+        else:
+            d['direction'] = str(position_direction)
 
-		#test market value
-		if market is None:
-			print ("\nError, please enter a market parameter value. Remember that you can also fetch available market symbol using getMarkets().\n")
-			return
-		else:
-			d['market'] = str(market)
+        # test market value
+        if market is None:
+            print("\nError, please enter a market parameter value. Remember that you can also fetch available market symbol using getMarkets().\n")
+            return
+        else:
+            d['market'] = str(market)
 
-		#test position_type value
-		if position_type is None:
-			print ("\nError, please enter a position_type parameter value. Can be '1min' or '5min' .\n")
-			return
-		else:
-			d['type'] = str(position_type)
+        # test position_type value
+        if position_type is None:
+            print(
+                "\nError, please enter a position_type parameter value. Can be '1min' or '5min' .\n")
+            return
+        else:
+            d['type'] = str(position_type)
 
-		#test size value
-		if size is None:
-			print ("\nError, please enter a size parameter value. Should be a turbo position’s size, in satoshis.  .\n")
-			return
-		else:
-			d['size'] = size
+        # test size value
+        if size is None:
+            print("\nError, please enter a size parameter value. Should be a turbo position’s size, in satoshis.  .\n")
+            return
+        else:
+            d['size'] = size
 
-		#test key parameter value is an accepted input
-		test0 =  self._updateKey(key)
-		if test0[0] is True:
-			key = test0[1]
-		else:
-			return
+        # test key parameter value is an accepted input
+        test0 = self._updateKey(key)
+        if test0[0] is True:
+            key = test0[1]
+        else:
+            return
 
-		#Create request elements
-		url = self.start_url+'position-turbo/new'
-		h = {"Authorization":"Bearer "+key,"Partner-Id":"fAoRwgvNoQjekD3Hk"}
-		r = requests.post(url, headers=h, data=d)
+        # Create request elements
+        url = self.start_url+'position-turbo/new'
+        h = {"Authorization": "Bearer "+key, "Partner-Id": "fAoRwgvNoQjekD3Hk"}
+        r = requests.post(url, headers=h, data=d)
 
-		if self.verbose:
-			print ('\nOpening a turbo position: \n')
+        if self.verbose:
+            print('\nOpening a turbo position: \n')
 
-		return self._checkResp(r)
+        return self._checkResp(r)
 
-	def getTurboPosition(self, position_id=None, key = None):
-		"""
-		Fetch information about an existing turbo position.
+    def getTurboPosition(self, position_id=None, key=None):
+        """
+        Fetch information about an existing turbo position.
 
-		arg:
-		----
-		position_id 	unique 	 Required.  ID that identify your position
-		key		string 	 Optional.  One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
+        arg:
+        ----
+        position_id 	unique 	 Required.  ID that identify your position
+        key		string 	 Optional.  One API token to use in order to send the request, could either be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
 
-		resp:
-		-----
-		id 		string   	Unique ID for the turbo position.
-		contract_id	string	 	ID of the contract this turbo position belongs to.
-		direction 	string 	 	Can be long or short.
-		market 		string 		Market this turbo position was executed on.
-		state 		string 		Can be active, or closed.
-		size 		integer 	Position’s size, in satoshis.
-		entry_price 	number 		Price at which the turbo position was executed.
-		payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
-		close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
-		profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
-		created_at 	integer 	When the position was created.
-		closed_at 	integer 	When the position was closed. Appears only if the position is closed.
-		currency 	string 		Base currency.
+        resp:
+        -----
+        id 		string   	Unique ID for the turbo position.
+        contract_id	string	 	ID of the contract this turbo position belongs to.
+        direction 	string 	 	Can be long or short.
+        market 		string 		Market this turbo position was executed on.
+        state 		string 		Can be active, or closed.
+        size 		integer 	Position’s size, in satoshis.
+        entry_price 	number 		Price at which the turbo position was executed.
+        payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
+        close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
+        profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
+        created_at 	integer 	When the position was created.
+        closed_at 	integer 	When the position was closed. Appears only if the position is closed.
+        currency 	string 		Base currency.
 
-		"""
+        """
 
-		#test position_direction value
-		if position_id is None:
-			print ("\nError, please enter a position_id parameter value.\n")
-			return
+        # test position_direction value
+        if position_id is None:
+            print("\nError, please enter a position_id parameter value.\n")
+            return
 
-		#test key parameter value is an accepted input
-		test0 =  self._updateKey(key)
-		if test0[0] is True:
-			key = test0[1]
-		else:
-			return
+        # test key parameter value is an accepted input
+        test0 = self._updateKey(key)
+        if test0[0] is True:
+            key = test0[1]
+        else:
+            return
 
-		#Create request elements
-		url = self.start_url+'position-turbo/'+ position_id
-		h = {"Authorization":"Bearer "+key}
-		r = requests.get(url, headers=h)
+        # Create request elements
+        url = self.start_url+'position-turbo/' + position_id
+        h = {"Authorization": "Bearer "+key}
+        r = requests.get(url, headers=h)
 
-		if self.verbose:
-			print ('\nTurbo position informations: \n')
+        if self.verbose:
+            print('\nTurbo position informations: \n')
 
-		return self._checkResp(r)
+        return self._checkResp(r)
 
-	def listturboPositions(self, position_state='active',limit=5, key=None):
-		"""
-		List turbo positions.
-		Use this function to request a list of turbo positions. state can be active or closed. Defaults to activeself. 
-		Active positions are sorted by created_at and closed positions are sorted by closed_at.
+    def listturboPositions(self, position_state='active', limit=5, key=None):
+        """
+        List turbo positions.
+        Use this function to request a list of turbo positions. state can be active or closed. Defaults to activeself. 
+        Active positions are sorted by created_at and closed positions are sorted by closed_at.
 
-		args:
-		-----
-		position_state 	string 	Optional. State can be 'active', or 'closed'. Defaults to active
-		limit 		integer Optional. Number of results per request. Defaults to 5. Max is 30.
-		key     	string 	Optional. One API token to use, could be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
+        args:
+        -----
+        position_state 	string 	Optional. State can be 'active', or 'closed'. Defaults to active
+        limit 		integer Optional. Number of results per request. Defaults to 5. Max is 30.
+        key     	string 	Optional. One API token to use, could be 'BTC_real_key', 'BTC_demo_key', 'DASH_real_key' or 'DASH_demo_key'. DEFAULT is BTC_demo_key
 
-		resp:
-		----
-		id 		string   	Unique ID for the turbo position.
-		contract_id	string	 	ID of the contract this turbo position belongs to.
-		direction 	string 	 	Can be long or short.
-		market 		string 		Market this turbo position was executed on.
-		state 		string 		Can be active, or closed.
-		size 		integer 	Position’s size, in satoshis.
-		entry_price 	number 		Price at which the turbo position was executed.
-		payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
-		close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
-		profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
-		created_at 	integer 	When the position was created.
-		closed_at 	integer 	When the position was closed. Appears only if the position is closed.
-		currency 	string 		Base currency.
-		
-		"""	
-		d = {}
+        resp:
+        ----
+        id 		string   	Unique ID for the turbo position.
+        contract_id	string	 	ID of the contract this turbo position belongs to.
+        direction 	string 	 	Can be long or short.
+        market 		string 		Market this turbo position was executed on.
+        state 		string 		Can be active, or closed.
+        size 		integer 	Position’s size, in satoshis.
+        entry_price 	number 		Price at which the turbo position was executed.
+        payoff 		number 		Payoff in case of correct prediction. Multiply by size to get payoff in satoshis.
+        close_price 	number 		Price at which the position was closed. Appears only if the position is closed.
+        profit 		number 		Profit made on the trade, in satoshis. Is negative in case of loss. Appears only if the position is closed.
+        created_at 	integer 	When the position was created.
+        closed_at 	integer 	When the position was closed. Appears only if the position is closed.
+        currency 	string 		Base currency.
 
-		#test limit parameter value
-		if limit < 1 or limit > 30:
-			print ('\nError, limit parameter value should be between 0 and 30')
-			return
-		else:
-			d['limit'] = limit
+        """
+        d = {}
 
-		#test position_state parameter value
-		if position_state not in ['active', 'closed']:
-			print ("\nError, position_state parameter value should be either 'active', or 'closed'")
-			return
+        # test limit parameter value
+        if limit < 1 or limit > 30:
+            print('\nError, limit parameter value should be between 0 and 30')
+            return
+        else:
+            d['limit'] = limit
 
-		#test key parameter value is an accepted input
-		test0 =  self._updateKey(key)
-		if test0[0] is True:
-			key = test0[1]
-		else:
-			return
+        # test position_state parameter value
+        if position_state not in ['active', 'closed']:
+            print(
+                "\nError, position_state parameter value should be either 'active', or 'closed'")
+            return
 
-		#Create request elements
-		url = self.start_url+'positions-turbo/'+ position_state
-		h = {"Authorization":"Bearer "+key}
-		r = requests.get(url,headers=h,data = d)
+        # test key parameter value is an accepted input
+        test0 = self._updateKey(key)
+        if test0[0] is True:
+            key = test0[1]
+        else:
+            return
 
-		if self.verbose:
-			print ('\nListing all turbo positions: \n')
+        # Create request elements
+        url = self.start_url+'positions-turbo/' + position_state
+        h = {"Authorization": "Bearer "+key}
+        r = requests.get(url, headers=h, data=d)
 
-		return self._checkResp(r)
+        if self.verbose:
+            print('\nListing all turbo positions: \n')
+
+        return self._checkResp(r)
